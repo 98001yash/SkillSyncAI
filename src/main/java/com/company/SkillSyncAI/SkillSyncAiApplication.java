e8c1b30dc05a17fed005e19b258f9eba544bf900
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-02 09:10:06 +0530

    added the application.properties file

---
+++ src/main/java/com/company/SkillSyncAI/SkillSyncAiApplication.java
@@ -0,0 +1,13 @@
+package com.company.SkillSyncAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSyncAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillSyncAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/SkillSyncAI/SkillSyncAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.SkillSyncAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSyncAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
